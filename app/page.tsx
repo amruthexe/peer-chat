a70b3798906c301f9636cb53474205f66cd173c5
amruthexe <aluriamruthrajit@gmail.com>
2025-01-13 14:00:05 +0530

    all

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
 import { ChatApp } from "@/components/custom/chat-box";
 
 export default function Home() {
-  const [peerId, setPeerId] = useState("");
-
-  useEffect(() => {
-    // Generate the peerId only on the client side
-    const id = Math.random().toString(36).substr(2, 9);
-    setPeerId(id);
-  }, []);
-
-  // Wait for the peerId to be generated before rendering the ChatApp
-  if (!peerId) return null;
-
+  const peerId = Math.random().toString(36).substr(2, 9);
   return (
     <div className="App">
       <ChatApp peerId={peerId} />
